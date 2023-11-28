ab4496300e74971655c63a5ee610cc4da30c9d08
eren ozcan <erenozcandev@gmail.com>
2023-11-28 15:59:47 +0300

    add spa section

--- src/app/spa/page.js
+++ src/app/spa/page.js
@@ -14,9 +14,265 @@ export default function Page() {
               className="h-[180px] w-[100%] object-center object-cover"
             />
             <div class="absolute top-1/3 my-auto text-center px-4 w-full">
-              <h1 class="text-white text-5xl text-stroke-3 uppercase">
-                SPA
+              <h1 class="text-white text-5xl text-stroke-3 uppercase">SPA</h1>
+            </div>
+          </div>
+          <div className="mx-auto container pt-16">
+            <h1 className="text-[#0d3c5a] text-3xl">SPA</h1>
+            <div className="flex flex-row gap-x-12">
+              <div className="w-[1200px]">
+                <p className="pt-6 text-lg leading-relaxed">
+                  Loryma Resortun yeni spa ve hamam bölümü, yanındaki kapalı
+                  havuz alanı da sayıldığında yaklaşık 400 m2lik bir alanda sizi
+                  dinçleştirecek ve tatilinizden artı değer kazandıracak türlü
+                  program ve uygulamalarla hizmetinizdedir. Deneyimli
+                  partnerimiz Loryma Spa, doğu ve batı tarzı klasik masaj
+                  seçenekleri, özel detoks, anti-aging, stres yönetimi,
+                  gençleştirme, yüz ve vücut bakımı programıyla Loryma
+                  misafirlerinin konforunu sağlamakta ve ruh-zihin-beden
+                  sağlığını desteklemektedir.
+                  <br className="mb-8" />
+                  Loryma Spa Loryma Resort’un doğasever yanaşımına uyumlu
+                  biçimde tasarladığı programında bal, süt, şifalı bitkiler,
+                  yosunlar, aromatik yağlar ve Loryma Resortun kendi üretimi
+                  zeytinyağlarını kullanmaktadır. Spanın bölümleri arasında
+                  geleneksel Türk hamamı, tek ve çift kişilik masaj odaları, bir
+                  adet tylo saunası, bir tuz odası ve konforlu dinlenme alanları
+                  bulunmaktadır.
+                </p>
+              </div>
+              <div className="flex flex-col gap-y-4 justify-center">
+                <Image
+                  src="https://i.hizliresim.com/guxwrnl.jpg"
+                  alt="Picture of the author"
+                  width={500}
+                  height={500}
+                  className="rounded-lg h-[300px] w-[500px] object-center object-cover"
+                />
+              </div>
+            </div>
+            <div className="max-w-[1400px] mx-auto">
+              <h1 className="text-center text-4xl text-[#0d3c5a] my-12">
+                GALERİ
               </h1>
+              <div className="grid grid-cols-4 gap-x-24 gap-y-6 mx-auto">
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+                <div className="w-[425px] h-[250px]">
+                  <Image
+                    src="https://picsum.photos/200"
+                    alt="Picture of the author"
+                    width={425}
+                    height={200}
+                    className="rounded-lg w-[325px] h-[250px] object-center object-cover"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
